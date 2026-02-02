b556db336334eb3489ce30aa21d1806aab95fb40
hidlow <vndtnot@gmail.com>
2026-02-02 18:32:58 +0500

    Add files via upload

--- main/bootstrapper.py
+++ main/bootstrapper.py
@@ -1,154 +1,193 @@
-# tg: @Hidlow
-# github: https://github.com/hiikikomorii
-# советую запускать скрипт с автоматическим закрытием консоли (/c or pause in .bat) чтобы не висел лишний раз
+"""made by @hidlow"""
 
-#основные модули, их не трогаем
+# импорт зависимостей
 import subprocess
 import sys
-import ctypes
+import tkinter as tk
+import os
+import threading
 
-# тут надо заменить на ваши импорты (желательно охватывать все возможные модули из проекта, а не только из main файла)
-try:
-    # пример
-    import colorama
+# класс, который обновляет и устанавливает pip
+class Pip:
+    def __init__(self, label_ru, label_eng, label_pipudp, main):
+        self.label_ru = label_ru
+        self.label_eng = label_eng
+        self.label_pipudp = label_pipudp
+        self.main = main
 
-    # если все модули пройдут проверку, запускается ваш main файл
-    hidlow_path = "example.py"
-    subprocess.Popen(
-        ["cmd", "/k", sys.executable, str(hidlow_path)],
-        creationflags=subprocess.CREATE_NEW_CONSOLE
-    )
-    sys.exit()
-
-except ModuleNotFoundError as e:
-    # в случае если вы используете свои импортируемые модули
-    # для того, чтобы проверить целостность файла(ов)
-    if e.name == "Ваш.модуль.из.сборки":
-        ctypes.windll.user32.MessageBoxW(0, f"Сборка повреждена\nпуть {e.name} не найден\nПроверьте совместимость сборки", "Error", 0x10)
-        sys.exit()
-
-
-    #boottraper запускается только тогда, когда скрипт ловит ModuleNotFoundError
-
-    from datetime import date, datetime
-    import platform
-    import os
-    import threading
-    import tkinter as tk
-
-    # настройка tkinter
-    root = tk.Tk()
-    root.title("BOOT-LOADER-NOMODULE")
-    root.attributes('-fullscreen', True)
-    root.config(bg="blue")
-
-    # для label_info
-    pybuild = platform.python_build()
-    pycomp = platform.python_compiler()
-    pyver = platform.python_version()
-    fullscreen = True
-    time1 = datetime.now().strftime("%H:%M:%S")
-    data1 = date.today()
-
-    #выход
-    def exit_bbt():
-        root.destroy()
-        sys.exit()
-
-    # текст для label_help
-    text = ("""
-Boottraped-menu\n
-Скрипту не удалось запуститься из-за ошибки\n
-Установите модули нажав кнопку 'Install'\n
-Обновите pip нажав кнопку 'upgrade pip'\n
-Нажав кнопку 'reboot' вы перезапускаете скрипт\n
-если ошибок не возникнет - скрипт, который
-вы пытались запустить, запустится без этого меню.
-                   """)
-
-    # перезазрузка
-    def reboot_bsod():
-        script_path = os.path.abspath(__file__)
-        subprocess.Popen(
-            ["cmd", "/c", sys.executable, str(script_path)],
-            creationflags=subprocess.CREATE_NEW_CONSOLE
-        )
-        sys.exit()
-        root.destroy()
-
-
-    # установка отсутвующего модуля
-    def install_pip_cmd():
-        label_ru.config(text=f"Подождите..", fg="white")
-        label_ru.pack(anchor="nw", pady=5)
-        label_eng.config(text=f"Wait..", fg="white")
-        label_eng.pack(anchor="nw", pady=5)
-        if e.name == "PIL":
+    # установка
+    def install_pip_cmd(self):
+        self.label_ru.config(text=f"Подождите..", fg="white")
+        self.label_ru.pack(anchor="nw", pady=5)
+        self.label_eng.config(text=f"Wait..", fg="white")
+        self.label_eng.pack(anchor="nw", pady=5)
+        if self.main.missing_module_name == "PIL":
             subprocess.run([sys.executable, "-m", "pip", "install", "Pillow"])
             return
         try:
-            result = subprocess.run([sys.executable, "-m", "pip", "install", e.name], capture_output=True, text=True)
+            result = subprocess.run([sys.executable, "-m", "pip", "install", self.main.missing_module_name], capture_output=True, text=True)
             if result.returncode == 0:
-                label_ru.config(text=f"pip {e.name} Установлено!", fg="#00CF00")
-                label_ru.pack(anchor="nw", pady=5)
-                label_eng.config(text=f"pip {e.name} it was established", fg="#00CF00")
-                label_eng.pack(anchor="nw", pady=5)
+                self.label_ru.config(text=f"pip {self.main.missing_module_name} Установлено!", fg="#00CF00")
+                self.label_ru.pack(anchor="nw", pady=5)
+                self.label_eng.config(text=f"pip {self.main.missing_module_name} it was established", fg="#00CF00")
+                self.label_eng.pack(anchor="nw", pady=5)
 
             if result.returncode != 0:
                 error_msg = result.stderr
-                label_ru.config(text=f"Ошибка pip: {error_msg}", fg="red")
-                label_ru.pack(anchor="nw", pady=5)
-                label_eng.config(text=f"error pip: {error_msg}", fg="red")
-                label_eng.pack(anchor="nw", pady=5)
-
+                self.label_ru.config(text=f"Ошибка pip: {error_msg}", fg="red")
+                self.label_ru.pack(anchor="nw", pady=5)
+                self.label_eng.config(text=f"error pip: {error_msg}", fg="red")
+                self.label_eng.pack(anchor="nw", pady=5)
 
         except Exception as error_pip_cmd:
-           print(error_pip_cmd)
+            print(error_pip_cmd)
 
-    # апдейд pip до последней версии
-    def upd_pip_cmd():
-        label_pipudp.config(text="wait...")
-        label_pipudp.pack(anchor="sw", pady=5, padx=20)
+    # апдейт
+    def upd_pip_cmd(self):
+        self.label_pipudp.config(text="wait...")
+        self.label_pipudp.pack(anchor="sw", pady=5, padx=20)
         try:
             result = subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip"], capture_output=True, text=True)
             if result.returncode == 0:
                 out_msg = result.stdout
-                label_pipudp.config(text=f"pip successfully updated\n{out_msg}", fg="#00CF00")
-                label_pipudp.pack(anchor="sw", pady=5, padx=20)
+                self.label_pipudp.config(text=f"pip successfully updated\n{out_msg}", fg="#00CF00")
+                self.label_pipudp.pack(anchor="sw", pady=5, padx=20)
             if result.returncode != 0:
                 error_msg = result.stderr
-                label_pipudp.config(text=f"pip upd error: {error_msg}", fg="red")
-                label_pipudp.pack(anchor="sw", pady=5, padx=20)
+                self.label_pipudp.config(text=f"pip upd error: {error_msg}", fg="red")
+                self.label_pipudp.pack(anchor="sw", pady=5, padx=20)
 
         except Exception as error_updpip:
             print(error_updpip)
 
-    # отдельный поток для того чтобы gui не завис
-    def thread_install_pip_cmd():
-        threading.Thread(target=install_pip_cmd).start()
-    def thread_upd_pip_cmd():
-        threading.Thread(target=upd_pip_cmd).start()
+# bootstrapper gui
+class Main:
+    def __init__(self, master):
+        import platform
 
-    # полноэкранный режим
-    def fullscreen_on():
-        global fullscreen
-        fullscreen = not fullscreen
+        # переменные
+        self.missing_module_name = "none"
+        self.bg_state = 1
+        self.fullscreen = False
+        self.reboot_path = os.path.abspath(__file__)
+        self.pybuild = platform.python_build()
+        self.pycomp = platform.python_compiler()
+        self.pyver = platform.python_version()
 
-        if fullscreen:
-            root.attributes('-fullscreen', True)
+        self.text = ("""
+        Bootstrapped-menu\n
+        Script startup failed.
+        Install: Download missing modules.
+        Upgrade pip: Update pip to the latest version.
+        Reboot: Restart the script.
+        The script will bypass this menu if it runs successfully.""")
+
+        # запуск проверки модулей (check_pip)
+        try:
+            self.check_pip()
+        except Exception:
+            pass
+
+        # настройка gui
+        self.root = master
+        self.root.title("BOOTSTRAPPER-NOMODULE")
+        self.root.config(bg="blue")
+        self.root.geometry("976x635")
+
+        self._all_widgets()
+        self.use_pip = Pip(self.label_ru, self.label_eng, self.label_pipudp, self)
+
+    # все виджеты в gui
+    def _all_widgets(self):
+        self.frame = tk.Frame(self.root, bg="blue")
+        self.frame.pack(padx=20, anchor="nw")
+
+        self.frame2 = tk.Frame(self.root, bg="blue")
+        self.frame2.pack(padx=20, anchor="nw")
+
+        self.info_frame = tk.Frame(self.root, bg="blue")
+        self.info_frame.pack(side="right", pady=20, padx=20, anchor="se")
+
+        self.btnfullscr = tk.Button(self.frame2, text="Fullscreen", activebackground="#0010A7", bg="blue", fg="white",command=self.fullscreen_on, width=10)
+        self.btnfullscr.pack(side="left", padx=5, pady=5)
+
+        self.theme_btn = tk.Button(self.frame2, text="Theme", activebackground="#0010A7", bg="blue", fg="white", command=self.theme_tk, width=10)
+        self.theme_btn.pack(side="left", padx=5, pady=5)
+
+        self.label_help = tk.Label(self.info_frame, text=self.text, fg="white", bg="blue", font=("bold", 15))
+        self.label_help.pack(pady=10)
+
+        self.labelinfo = tk.Label(self.info_frame, text=f"version: {self.pyver}\ncompiler: {self.pycomp}\nbuild: {self.pybuild}", fg="white", bg="blue", font=("bold", 15))
+        self.labelinfo.pack(pady=5)
+
+        self.label_ru = tk.Label(self.frame, fg="white", bg="blue", font=("bold", 15), text=f"Отсутствующие модули: {self.missing_module_name}", justify="left", anchor="nw")
+        self.label_ru.pack(anchor="nw", pady=5)
+
+        self.label_eng = tk.Label(self.frame, fg="white", bg="blue", font=("bold", 15), text=f"Missing modules: {self.missing_module_name}", justify="left", anchor="nw")
+        self.label_eng.pack(anchor="nw", pady=5)
+
+        self.label_pipudp = tk.Label(self.root, fg="white", bg="blue", font=("bold", 15), text=f"", justify="left", anchor="sw")
+
+        self.btn_exit = tk.Button(self.frame, text="Exit", activebackground="#0010A7", bg="blue", fg="white", command=lambda: exit(), width=10)
+        self.btn_exit.pack(side="left", padx=5, pady=5)
+
+        self.btn_reboot = tk.Button(self.frame, text="Reboot", activebackground="#0010A7", bg="blue", fg="white", command=self.reboot_bsod, width=10)
+        self.btn_reboot.pack(side="left", padx=5, pady=5)
+
+        self.btn_download = tk.Button(self.frame, text="Install", activebackground="#0010A7", bg="blue", fg="white", command=lambda: threading.Thread(target=self.use_pip.install_pip_cmd).start(),width=10)
+        self.btn_download.pack(side="left", padx=5, pady=5)
+
+        self.btn_pip = tk.Button(self.frame, text="upgrade pip", activebackground="#0010A7", bg="blue", fg="white", command=lambda: threading.Thread(target=self.use_pip.upd_pip_cmd).start(), width=10)
+        self.btn_pip.pack(side="left", padx=5, pady=5)
+
+    # проверка модулей
+    def check_pip(self):
+        """
+        cwd по желанию,
+        /k - не закрывает окно консоли после выхода,
+        в main_path написать имя вашего файла который будет запускаться
+        """
+
+        try:
+            import colorama
+
+            main_path = "example.py"
+            subprocess.Popen(
+                ["cmd", "/k", sys.executable, str(main_path)],
+                cwd="",
+                creationflags=subprocess.CREATE_NEW_CONSOLE
+            )
+            sys.exit()
+
+        except ModuleNotFoundError as e:
+            self.missing_module_name = e.name
+
+    # перезапуск скрипта
+    def reboot_bsod(self):
+        subprocess.Popen(
+            ["cmd", "/c", sys.executable, str(self.reboot_path)],
+            creationflags=subprocess.CREATE_NEW_CONSOLE
+        )
+        sys.exit()
+        self.root.destroy()
+
+    # вкл\выкл полноэкранный режим
+    def fullscreen_on(self):
+        self.fullscreen = not self.fullscreen
+
+        if self.fullscreen:
+            self.root.attributes('-fullscreen', True)
         else:
-            root.attributes('-fullscreen', False)
-            root.geometry("960x540")
+            self.root.attributes('-fullscreen', False)
+            self.root.geometry("976x635")
 
-    bg_state = 1
-    # смена темы
-    def theme_tk():
-        global bg_state
-
-        if bg_state == 1:
-            #black
-            for frames in (frame, frame2, root, info_frame):
+    # смена темы. синий\черный\белый
+    def theme_tk(self):
+        if self.bg_state == 1:
+            # black
+            for frames in (self.frame, self.frame2, self.root, self.info_frame):
                 frames.config(bg="black")
+
                 for widget in frames.winfo_children():
                     if isinstance(widget, tk.Button):
                         widget.config(
@@ -156,34 +195,39 @@ Boottraped-menu\n
                             activebackground="#0F0F0F",
                             activeforeground="white"
                         )
+
                 for labels in frames.winfo_children():
                     if isinstance(labels, tk.Label):
                         labels.config(
                             bg="black",
                         )
-            bg_state = 2
-        elif bg_state == 2:
-            #white
-            for frames in (frame, frame2, root, info_frame):
+
+            self.bg_state = 2
+        elif self.bg_state == 2:
+            # white
+            for frames in (self.frame, self.frame2, self.root, self.info_frame):
                 frames.config(bg="white")
+
                 for widget in frames.winfo_children():
                     if isinstance(widget, tk.Button):
                         widget.config(
                             bg="#EBEBEB",
                             activebackground="#C3C3C3",
                             fg="black",
-                            activeforeground = "black"
+                            activeforeground="black"
                         )
+
                 for labels in frames.winfo_children():
                     if isinstance(labels, tk.Label):
                         labels.config(
                             bg="white",
                             fg="black"
                         )
-            bg_state = 3
+
+            self.bg_state = 3
         else:
-            #blue
-            for frames in (frame, frame2, root, info_frame):
+            # blue
+            for frames in (self.frame, self.frame2, self.root, self.info_frame):
                 frames.config(bg="blue")
                 for widget in frames.winfo_children():
                     if isinstance(widget, tk.Button):
@@ -199,42 +243,11 @@ Boottraped-menu\n
                             bg="blue",
                             fg="white"
                         )
-            bg_state = 1
+            self.bg_state = 1
 
-    # тут все фреймы
-    frame = tk.Frame(root, bg="blue")
-    frame.pack(padx=20, anchor="nw")
-
-    frame2 = tk.Frame(root, bg="blue")
-    frame2.pack(padx=20, anchor="nw")
-
-    info_frame = tk.Frame(root, bg="blue")
-    info_frame.pack(side="right", pady=20, padx=20, anchor="se")
-
-    # все button и label
-    btnfullscr = tk.Button(frame2, text="Fullscreen", activebackground="#0010A7", bg="blue", fg="white", command=fullscreen_on, width=10).pack(side="left", padx=5, pady=5)
-    theme_btn = tk.Button(frame2, text="Theme", activebackground="#0010A7", bg="blue", fg="white", command=theme_tk, width=10).pack(side="left", padx=5, pady=5)
-
-    label_time = tk.Label(info_frame, text=f"{data1} {time1}", fg="white", bg="blue", font=("bold", 12))
-    label_time.pack(pady=10)
-
-    label_help = tk.Label(info_frame, text=text, fg="white", bg="blue", font=("bold", 12))
-    label_help.pack(pady=10)
-
-    labelinfo = tk.Label(info_frame, text=f"version: {pyver}\ncompiler: {pycomp}\nbuild: {pybuild}", fg="white", bg="blue", font=("bold", 15))
-    labelinfo.pack(pady=5)
-
-    label_ru = tk.Label(frame, fg="white", bg="blue", font=("bold", 15), text=f"Отсутствующие модули: {e.name}", justify="left", anchor="nw")
-    label_ru.pack(anchor="nw", pady=5)
-
-    label_eng = tk.Label(frame, fg="white", bg="blue", font=("bold", 15), text=f"Missing modules: {e.name}",justify="left", anchor="nw")
-    label_eng.pack(anchor="nw", pady=5)
-
-    label_pipudp = tk.Label(root, fg="white", bg="blue", font=("bold", 15), text=f"", justify="left", anchor="sw")
-
-    btn_exit = tk.Button(frame, text="Exit", activebackground="#0010A7", bg="blue", fg="white", command=exit_bbt, width=10).pack(side="left", padx=5, pady=5)
-    btn_reboot = tk.Button(frame, text="Reboot", activebackground="#0010A7", bg="blue", fg="white", command=reboot_bsod, width=10).pack(side="left", padx=5, pady=5)
-    btn_download = tk.Button(frame, text="Install", activebackground="#0010A7", bg="blue", fg="white", command=thread_install_pip_cmd, width=10).pack(side="left", padx=5, pady=5)
-    btn_pip = tk.Button(frame, text="upgrade pip", activebackground="#0010A7", bg="blue", fg="white", command=thread_upd_pip_cmd, width=10).pack(side="left", padx=5, pady=5)
-
+# запуск
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Main(root)
     root.mainloop()
+
